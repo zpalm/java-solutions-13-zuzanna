db8ef1ec1776081c6f6bf79b57500f36c53f654a
Zuzanna Palmowska <zuzanna.palmowska@gmail.com>
2019-04-24 16:14:29 +0200

    Task #1 - edited

--- src/main/java/pl/coderstrust/foobar/FooBar.java
+++ src/main/java/pl/coderstrust/foobar/FooBar.java
@@ -8,15 +8,15 @@ public class FooBar {
 
     public static void fooBar(int firstNumber, int lastNumber) {
         for (int i = firstNumber; i <= lastNumber; i++) {
-            System.out.print(i);
-            if (i % 15 == 0) {
-                System.out.print(" FooBar");
-            } else if (i % 3 == 0) {
-                System.out.print(" Foo");
-            } else if (i % 5 == 0) {
-                System.out.print(" Bar");
+            StringBuilder sb = new StringBuilder();
+            sb.append(i).append(" ");
+            if (i % 3 == 0) {
+                sb.append("Foo");
             }
-            System.out.println();
+            if (i % 5 == 0) {
+                sb.append("Bar");
+            }
+            System.out.println(sb);
         }
     }
 }
