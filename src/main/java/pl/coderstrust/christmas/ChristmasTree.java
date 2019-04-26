a9a6aac02b408adbdf2b1667fac0f56f74d9482f
Zuzanna Palmowska <zuzanna.palmowska@gmail.com>
2019-04-26 17:08:57 +0200

    Task #3 - edited

--- src/main/java/pl/coderstrust/christmas/ChristmasTree.java
+++ src/main/java/pl/coderstrust/christmas/ChristmasTree.java
@@ -4,48 +4,29 @@ public class ChristmasTree {
     public static void main(String[] args) {
         printChristmasTree(5);
         System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
         printChristmasTree(7);
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
         System.out.println();
         printChristmasTree(10);
     }
 
     public static void printChristmasTree(int size) {
         if (size < 2) {
-            System.out.println("Invalid size");
+            System.out.println("Invalid size.");
         }
-        int row = 1;
-        int column = 1;
-        for (int i = column; i < (size + row); i++) {
-            if (row == size) {
-                for (i = 1; i <= size; i++) {
-                    if (i <= (size - 2)) {
-                        System.out.print(" ");
-                        continue;
-                    }
-                    System.out.print("*");
-                }
-                break;
-            }
-            if (i <= (size - row)) {
+        for (int i = 1; i < size; i++) {
+            for (int j = 1; j < (size - i); j++) {
                 System.out.print(" ");
             }
-            if (i > (size - row) && i < size + (row - 1)) {
+            for (int j = (size - i); j < (i + size - 1); j++) {
                 System.out.print("*");
             }
-            if (i == size + (row - 1)) {
-                System.out.print("*");
-                System.out.println();
-                row++;
-                i = 0;
-            }
+            System.out.println();
+        }
+        for (int i = 1; i < (size - 2); i++) {
+            System.out.print(" ");
+        }
+        for (int i = size - 2; i < size; i++) {
+            System.out.print("*");
         }
     }
 }
