fd9e013142c962308014d1aab901b07e128ef33b
Zuzanna Palmowska <zuzanna.palmowska@gmail.com>
2019-04-27 17:41:09 +0200

    Task #2 - Multiplication Table

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -10,28 +10,25 @@ public class MultiplicationTable {
         printMultiplicationTable(10);
         System.out.println();
         printMultiplicationTable(12);
-
     }
 
     public static void printMultiplicationTable(int size) {
-        if (size >= 1 && size <= 12) {
-            System.out.printf("%6s", " ");
-            for (int i = 1; i <= size; i++) {
-                System.out.printf("%-6d", i);
+        if (size < 1 || size > 12) {
+            System.out.println("Invalid value.");
+        }
+        System.out.printf("%6s", " ");
+        for (int i = 1; i <= size; i++) {
+            System.out.printf("%-6d", i);
+        }
+        System.out.println();
+        System.out.println();
+        for (int i = 1; i <= size; i++) {
+            System.out.printf("%-6d", i);
+            for (int j = 1; j <= size; j++) {
+                System.out.printf("%-6d", j * i);
             }
             System.out.println();
             System.out.println();
-            int columnNumber = 0;
-            for (int i = 1; i <= size; i++) {
-                System.out.printf("%-6d", i);
-                columnNumber++;
-                for (int j = columnNumber; j <= size; j++) {
-                    System.out.printf("%-6d", j * i);
-                }
-                System.out.println();
-                System.out.println();
-                columnNumber = 0;
-            }
         }
     }
 }
