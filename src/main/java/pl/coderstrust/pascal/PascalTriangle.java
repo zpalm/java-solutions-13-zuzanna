d9b0ce355ec060ede0d5979c2bfae3dc1544f830
Zuzanna Palmowska <zuzanna.palmowska@gmail.com>
2019-04-28 16:01:12 +0200

    Task #4 - Pascal Triangle

--- src/main/java/pl/coderstrust/pascal/PascalTriangle.java
+++ src/main/java/pl/coderstrust/pascal/PascalTriangle.java
@@ -5,51 +5,33 @@ public class PascalTriangle {
     public static void main(String[] args) {
         printPascalTriangle(4);
         System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
         printPascalTriangle(7);
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
         System.out.println();
         printPascalTriangle(11);
     }
 
     public static void printPascalTriangle(int number) {
         if (number < 1) {
-            System.out.println("Invalid Value");
+            throw new IllegalArgumentException("Number must be greater than 0.");
         }
-        int pascalTerm = 0;
-        int column = 0;
+        long pascalTerm = 0;
         for (int i = 0; i < number; i++) {
+            for (int j = 0; j < (number - i); j++) {
+                System.out.print("   ");
+            }
             for (int j = 0; j <= i; j++) {
-                for (int k = column; k <= (number - i) - 1; k++) {
-                    if (j >= 1) {
-                        break;
-                    }
-                    System.out.print("  ");
-                }
                 pascalTerm = factorial(i) / (factorial(j) * factorial(i - j));
-                if (pascalTerm < 10) {
-                    System.out.print(pascalTerm + "   ");
-                } else if (pascalTerm >= 10 && pascalTerm <= 99) {
-                    System.out.print(pascalTerm + "  ");
-                } else if (pascalTerm >= 100) {
-                    System.out.print(pascalTerm + " ");
-                }
+                System.out.printf("%-6d", pascalTerm);
             }
             System.out.println();
         }
     }
 
-    public static int factorial(int number) {
-        int factorial = 1;
+    public static long factorial(int number) {
+        long result = 1;
         for (int i = 1; i <= number; i++) {
-            factorial = factorial * i;
+            result = result * i;
         }
-        return factorial;
+        return result;
     }
 }
