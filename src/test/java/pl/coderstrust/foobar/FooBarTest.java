8287fe02492a708325a07a4982f335bab074ad86
Zuzanna Palmowska <zuzanna.palmowska@gmail.com>
2019-05-13 15:24:21 +0200

    Task #10 - FooBar tests

--- src/test/java/pl/coderstrust/foobar/FooBarTest.java
+++ src/test/java/pl/coderstrust/foobar/FooBarTest.java
@@ -1,38 +1,17 @@
 package pl.coderstrust.foobar;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class FooBarTest {
 
-    private FooBar foobar;
-    private String[] actualArray;
-
-    @Before
-    public void setUp() {
-        foobar = new FooBar();
-        actualArray = foobar.fooBar(0, 100);
-    }
-
     @Test
-    public void shouldReturnFooForThree() {
-        String actualResult = actualArray[3];
-        String expectedResult = "3 Foo";
-        Assert.assertEquals(expectedResult, actualResult);
-    }
-
-    @Test
-    public void shouldReturnBarForFive() {
-        String actualResult = actualArray[5];
-        String expectedResult = "5 Bar";
-        Assert.assertEquals(expectedResult, actualResult);
-    }
-
-    @Test
-    public void shouldReturnFooBarForFifteen() {
-        String actualResult = actualArray[15];
-        String expectedResult = "15 FooBar";
-        Assert.assertEquals(expectedResult, actualResult);
+    public void shouldReturnTwentyFirstResults() {
+        String[] actualResult = FooBar.fooBar(0, 20);
+        String[] expectedResult = {"0 FooBar", "1 ", "2 ", "3 Foo",
+                "4 ", "5 Bar", "6 Foo", "7 ", "8 ", "9 Foo", "10 Bar", "11 ",
+                "12 Foo", "13 ", "14 ", "15 FooBar", "16 ", "17 ", "18 Foo",
+                "19 ", "20 Bar"};
+        Assert.assertArrayEquals(expectedResult, actualResult);
     }
 }
