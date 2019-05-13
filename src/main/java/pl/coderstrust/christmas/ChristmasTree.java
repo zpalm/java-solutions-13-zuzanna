8cd3f2c10ad9f17f5f1f7a115f7e14f950b3dbe6
Zuzanna Palmowska <zuzanna.palmowska@gmail.com>
2019-05-13 18:09:53 +0200

    Task #12 - Christmas Tree tests

--- src/main/java/pl/coderstrust/christmas/ChristmasTree.java
+++ src/main/java/pl/coderstrust/christmas/ChristmasTree.java
@@ -1,32 +1,39 @@
 package pl.coderstrust.christmas;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ChristmasTree {
     public static void main(String[] args) {
-        printChristmasTree(5);
-        System.out.println();
-        printChristmasTree(7);
-        System.out.println();
-        printChristmasTree(10);
+        System.out.println(Arrays.toString(printChristmasTree(5)));
+        System.out.println(Arrays.toString(printChristmasTree(7)));
+        System.out.println(Arrays.toString(printChristmasTree(10)));
     }
 
-    public static void printChristmasTree(int size) {
-        if (size < 2) {
-            System.out.println("Invalid size.");
+    public static String[] printChristmasTree(int size) {
+        if (size < 3) {
+            System.out.println("Size must be greater than 2.");
+            throw new IllegalArgumentException();
         }
+        List<String> result = new ArrayList<>();
+        StringBuilder christmasTree = new StringBuilder();
         for (int i = 1; i < size; i++) {
             for (int j = 1; j < (size - i); j++) {
-                System.out.print(" ");
+                christmasTree.append(" ");
             }
             for (int j = (size - i); j < (i + size - 1); j++) {
-                System.out.print("*");
+                christmasTree.append("*");
             }
-            System.out.println();
+            christmasTree.append(System.getProperty("line.separator"));
         }
         for (int i = 1; i < (size - 2); i++) {
-            System.out.print(" ");
+            christmasTree.append(" ");
         }
         for (int i = (size - 2); i < size; i++) {
-            System.out.print("*");
+            christmasTree.append("*");
         }
+        result.add(christmasTree.toString());
+        return result.toArray(new String[result.size()]);
     }
 }
---
+++ src/test/java/pl/coderstrust/christmas/ChristmasTreeTest.java
@@ -0,0 +1,24 @@
+package pl.coderstrust.christmas;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ChristmasTreeTest {
+
+    @Test
+    public void shouldPrintATreeOfSizeSix() {
+        String[] actual = ChristmasTree.printChristmasTree(6);
+        String[] expected = {"    *" + System.getProperty("line.separator") +
+                "   ***" + System.getProperty("line.separator") +
+                "  *****" + System.getProperty("line.separator") +
+                " *******" + System.getProperty("line.separator") +
+                "*********" + System.getProperty("line.separator") +
+                "   **"};
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldReturnInvalidValueMessage() {
+        ChristmasTree.printChristmasTree(1);
+    }
+}
