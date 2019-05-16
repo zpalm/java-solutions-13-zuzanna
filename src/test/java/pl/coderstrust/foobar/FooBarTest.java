c51a239448af477613ac92ec0448364b93184951
Zuzanna Palmowska <zuzanna.palmowska@gmail.com>
2019-05-16 11:52:40 +0200

    Task #10 - FooBar tests

--- src/test/java/pl/coderstrust/foobar/FooBarTest.java
+++ src/test/java/pl/coderstrust/foobar/FooBarTest.java
@@ -8,10 +8,29 @@ public class FooBarTest {
     @Test
     public void shouldReturnTwentyFirstResults() {
         String[] actualResult = FooBar.fooBar(0, 20);
-        String[] expectedResult = {"0 FooBar", "1 ", "2 ", "3 Foo",
-                "4 ", "5 Bar", "6 Foo", "7 ", "8 ", "9 Foo", "10 Bar", "11 ",
-                "12 Foo", "13 ", "14 ", "15 FooBar", "16 ", "17 ", "18 Foo",
-                "19 ", "20 Bar"};
+        String[] expectedResult = {
+                "0 FooBar",
+                "1 ",
+                "2 ",
+                "3 Foo",
+                "4 ",
+                "5 Bar",
+                "6 Foo",
+                "7 ",
+                "8 ",
+                "9 Foo",
+                "10 Bar",
+                "11 ",
+                "12 Foo",
+                "13 ",
+                "14 ",
+                "15 FooBar",
+                "16 ",
+                "17 ",
+                "18 Foo",
+                "19 ",
+                "20 Bar"
+        };
         Assert.assertArrayEquals(expectedResult, actualResult);
     }
 }
