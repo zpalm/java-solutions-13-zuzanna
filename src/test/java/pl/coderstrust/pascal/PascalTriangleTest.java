40615236b19f4ab3da125b1d96055ae1dd4f8f69
Zuzanna Palmowska <zuzanna.palmowska@gmail.com>
2019-05-22 16:16:33 +0200

    Task #13 - Pascal triangle tests

--- src/test/java/pl/coderstrust/pascal/PascalTriangleTest.java
+++ src/test/java/pl/coderstrust/pascal/PascalTriangleTest.java
@@ -1,7 +1,9 @@
 package pl.coderstrust.pascal;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,20 +11,24 @@ import java.util.List;
 public class PascalTriangleTest {
 
     @Test
-    public void shouldReturnResultsForInputOfFive() {
-        List<String> actual = PascalTriangle.getPascalTriangle(5);
+    public void shouldReturnCorrectPascalTriangle() {
         List<String> expected = new ArrayList<>();
         expected.add("1 ");
         expected.add("1 1 ");
         expected.add("1 2 1 ");
         expected.add("1 3 3 1 ");
         expected.add("1 4 6 4 1 ");
+        List<String> actual = PascalTriangle.getPascalTriangle(5);
         Assert.assertEquals(expected, actual);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldReturnInvalidValueMessage() {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void shouldThrowExceptionForInvalidArgument() throws IllegalArgumentException {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Number must be greater than 0.");
         PascalTriangle.getPascalTriangle(0);
     }
-
 }
