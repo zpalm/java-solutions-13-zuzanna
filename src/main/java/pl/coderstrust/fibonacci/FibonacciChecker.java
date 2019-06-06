56d6bcefb700ef788ddfa41f1dd116e5b14c57ff
Zuzanna Palmowska <zuzanna.palmowska@gmail.com>
2019-06-06 18:12:42 +0200

    Task #17 - Is that a Fibonacci number?

--- src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciChecker.java
@@ -4,30 +4,21 @@ import java.util.HashMap;
 
 public class FibonacciChecker {
 
-    private static long N;
     private static HashMap<Long, Boolean> cache = new HashMap<Long, Boolean>();
 
     boolean isFibonacciNumber(long number) {
         if (!cache.containsKey(number)) {
-            return fibonacci(number);
+            cache.put(number, fibonacci(number));
         }
         return cache.get(number);
     }
 
     private boolean fibonacci(long number) {
-        N = number;
-        double i = (Math.log(number) + Math.log(Math.sqrt(5))) / Math.log((Math.sqrt(5) + 1) / 2);
-        if (fib(i)) {
-            cache.put(number, true);
+        double nthFibonacciNumber = (Math.log(number) + Math.log(Math.sqrt(5))) / Math.log((Math.sqrt(5) + 1) / 2);
+        if (Math.round(((Math.pow((Math.sqrt(5) + 1) / 2, (int) Math.round(nthFibonacciNumber))) / Math.sqrt(5))) == number) {
             return true;
+        } else {
+            return false;
         }
-        return false;
-    }
-
-    private boolean fib(double i) {
-        if (Math.round(((Math.pow((Math.sqrt(5) + 1) / 2, (int) Math.ceil(i))) / Math.sqrt(5))) == N || Math.round(((Math.pow((Math.sqrt(5) + 1) / 2, (int) Math.floor(i))) / Math.sqrt(5))) == N) {
-            return true;
-        }
-        return false;
     }
 }
--- src/test/java/pl/coderstrust/fibonacci/FibonacciCheckerTest.java
+++ src/test/java/pl/coderstrust/fibonacci/FibonacciCheckerTest.java
@@ -2,31 +2,39 @@ package pl.coderstrust.fibonacci;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class FibonacciCheckerTest {
 
-    @Test
-    public void shouldReturnTrueForFive() {
-        FibonacciChecker fibonacciChecker = new FibonacciChecker();
-        boolean actual = fibonacciChecker.isFibonacciNumber(5);
-        boolean expected = true;
-        Assert.assertEquals(expected, actual);
+    @Parameterized.Parameters
+    public static Collection isFibonacciNumber() {
+        Object[][] testResults = {
+                {0, true},
+                {1, true},
+                {4, false},
+                {8, true},
+                {9, false},
+                {1597, true},
+                {514220, false},
+                {10610209857723L, true}
+        };
+        return Arrays.asList(testResults);
     }
 
-    @Test
-    public void shouldReturnTrueForABigFibonacciNumber() {
-        FibonacciChecker fibonacciChecker = new FibonacciChecker();
-        boolean actual = fibonacciChecker.isFibonacciNumber(10610209857723L);
-        boolean expected = true;
-        Assert.assertEquals(expected, actual);
-    }
+    @Parameterized.Parameter
+    public long given;
+    @Parameterized.Parameter(1)
+    public boolean expected;
 
     @Test
-    public void shouldReturnFalseForFifteen() {
+    public void shouldReturnResult() {
         FibonacciChecker fibonacciChecker = new FibonacciChecker();
-        boolean actual = fibonacciChecker.isFibonacciNumber(15);
-        boolean expected = false;
-        Assert.assertEquals(expected, actual);
+        boolean result = fibonacciChecker.isFibonacciNumber(given);
+        Assert.assertEquals(expected, result);
     }
-
 }
