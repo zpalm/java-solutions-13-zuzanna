c6d71d1476b792ba2c0978b4065ce42158c3fc09
Zuzanna Palmowska <zuzanna.palmowska@gmail.com>
2019-06-14 18:47:45 +0200

    Task #19 - Numbers... from file

--- src/main/java/pl/coderstrust/processor/FileProcessor.java
+++ src/main/java/pl/coderstrust/processor/FileProcessor.java
@@ -1,6 +1,13 @@
 package pl.coderstrust.processor;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
