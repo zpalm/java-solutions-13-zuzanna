55aadf8d011a96d33417ca9ba859334fe8f87d7d
Zuzanna Palmowska <zuzanna.palmowska@gmail.com>
2019-06-16 22:48:10 +0200

    Task #26 - Do you love streams?

--- src/main/java/pl/coderstrust/processor/ProcessorWithStreams.java
+++ src/main/java/pl/coderstrust/processor/ProcessorWithStreams.java
@@ -4,22 +4,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 public class ProcessorWithStreams {
 
     public void process(String fileName, String resultFileName) throws IOException {
-        try (Stream<String> stream = Files.lines(Paths.get("src/test/resources/" + fileName))) {
-            byte[] result = stream
-                    .filter(line -> !line.matches("(.*)\\p{Alpha}(.*)") && line.trim().length() > 0)
-                    .map(line -> {
-                        String[] splitLine = line.trim().split("\\W+");
-                        int sum = Arrays.stream(splitLine).mapToInt(Integer::parseInt).sum();
-                        return String.join("+", splitLine).concat("=" + sum + "\n");
-                    })
-                    .reduce("", (s1, s2) -> s1 + s2)
-                    .getBytes();
-            Files.write(Paths.get("src/test/resources/" + resultFileName), result);
-        }
+        Files.write(Paths.get("src/test/resources/" + resultFileName), Files.lines(Paths.get("src/test/resources/" + fileName))
+                .filter(line -> !line.matches("(.*)\\p{Alpha}(.*)") && line.trim().length() > 0)
+                .map(line -> line.trim().split("\\W+"))
+                .map(splitLine -> {
+                    int sum = Arrays.stream(splitLine).mapToInt(Integer::parseInt).sum();
+                    return String.join("+", splitLine).concat("=" + sum + "\n");
+                })
+                .reduce("", (s1, s2) -> s1 + s2)
+                .getBytes()
+        );
     }
 }
