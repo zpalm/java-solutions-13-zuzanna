efe213d368c19843c4fedfdd98996e2fa9519c92
Zuzanna Palmowska <zuzanna.palmowska@gmail.com>
2019-06-16 22:22:28 +0200

    Task #26 - Do you love streams?

--- src/main/java/pl/coderstrust/processor/ProcessorWithStreams.java
+++ src/main/java/pl/coderstrust/processor/ProcessorWithStreams.java
@@ -1,11 +1,6 @@
 package pl.coderstrust.processor;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -14,20 +9,17 @@ import java.util.stream.Stream;
 public class ProcessorWithStreams {
 
     public void process(String fileName, String resultFileName) throws IOException {
-        File file = new File("src/test/resources/" + resultFileName);
-        try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
-             Stream<String> stream = Files.lines(Paths.get("src/test/resources/" + fileName))) {
-            stream.filter(line -> !line.matches("(.*)\\p{Alpha}(.*)") && line.trim().length() > 0)
-                    .forEach(line -> {
+        try (Stream<String> stream = Files.lines(Paths.get("src/test/resources/" + fileName))) {
+            byte[] result = stream
+                    .filter(line -> !line.matches("(.*)\\p{Alpha}(.*)") && line.trim().length() > 0)
+                    .map(line -> {
                         String[] splitLine = line.trim().split("\\W+");
                         int sum = Arrays.stream(splitLine).mapToInt(Integer::parseInt).sum();
-                        line = String.join("+", splitLine).concat("=" + sum);
-                        try {
-                            writer.write(line + "\n");
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    });
+                        return String.join("+", splitLine).concat("=" + sum + "\n");
+                    })
+                    .reduce("", (s1, s2) -> s1 + s2)
+                    .getBytes();
+            Files.write(Paths.get("src/test/resources/" + resultFileName), result);
         }
     }
 }
